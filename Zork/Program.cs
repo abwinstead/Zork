670db6840d48e2e976f7e3c5486f2b64537dd606
abwinstead <54908577+abwinstead@users.noreply.github.com>
2019-09-11 14:52:00 -0400

    4 - The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,6 +7,11 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
         }
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
     }
 }
